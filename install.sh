1f5b58981df5b3a573a476c779247098f9ba1239
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-11 23:03:27 +0530

    build(hyperv): install hot migration helper

--- install.sh
+++ install.sh
@@ -58,9 +58,6 @@ while true ; do
     esac
 done
 
-#-------------------------------------------------------------------------------
-# Definition of locations
-#-------------------------------------------------------------------------------
 if [ -z "$ROOT" ] ; then
     LIB_LOCATION="/usr/lib/one"
     SHARE_LOCATION="/usr/share/one"
@@ -79,25 +76,21 @@ fi
 
 LIB_DIRS="$LIB_LOCATION/ruby/cli/one_helper"
 MAN_LOCATION="/usr/share/man/man1"
-
 MAKE_DIRS="$BIN_LOCATION $SHARE_LOCATION $LIB_LOCATION $ETC_LOCATION
            $VAR_LOCATION $LIB_DIRS $SCRIPTS_LOCATION"
 
-#-------------------------------------------------------------------------------
-# FILE DEFINITION, WHAT IS GOING TO BE INSTALLED AND WHERE
-#-------------------------------------------------------------------------------
 INSTALL_FILES=(
     BIN_FILES:$BIN_LOCATION
-	ONE_CLI_LIB_FILES:$LIB_LOCATION/ruby/cli/one_helper
+    ONE_CLI_LIB_FILES:$LIB_LOCATION/ruby/cli/one_helper
     CONF_FILES:$ETC_LOCATION
     SCRIPTS_FILES:$SCRIPTS_LOCATION
 )
-
 
 BIN_FILES="oneswap oneswap-hyperv sesparse"
 ONE_CLI_LIB_FILES="esxi_client.rb \
                    esxi_vm.rb \
                    hyperv_helper.rb \
+                   hyperv_hot_helper.rb \
                    netapp_shift_helper.rb \
                    oneswap_helper.rb \
                    oneswap_logger.rb \
@@ -106,46 +99,35 @@ ONE_CLI_LIB_FILES="esxi_client.rb \
 CONF_FILES="oneswap.yaml"
 SCRIPTS_FILES="scripts/*"
 
-#-----------------------------------------------------------------------------
-# INSTALL.SH SCRIPT
-#-----------------------------------------------------------------------------
-
 if [ "$MANPAGE" = "yes" ]; then
-    # base document
     echo "# oneswap(1) -- OpenNebula OneSwap Tool" > oneswap.1.ronn
     echo >> oneswap.1.ronn
     $BIN_LOCATION/oneswap --help >> oneswap.1.ronn
-
-    # manual pages/html
     ronn --style toc --manual="oneswap(1) -- OpenNebula OneSwap Tool" oneswap.1.ronn
     gzip -c oneswap.1 > oneswap.1.gz
-
     cp oneswap.1.gz $MAN_LOCATION
     rm -f oneswap.1.ronn oneswap.1.gz
-
     exit 0
 fi
 
 for d in $MAKE_DIRS; do
-	mkdir -p $DESTDIR$d
+    mkdir -p $DESTDIR$d
 done
 
 INSTALL_SET="${INSTALL_FILES[@]}"
 
 do_file() {
-	if [ "$LINK" = "yes" ]; then
-		ln -s $SRC_DIR/$1 $DESTDIR$2
-	else
-		cp -RL $SRC_DIR/$1 $DESTDIR$2
-	fi
+    if [ "$LINK" = "yes" ]; then
+        ln -s $SRC_DIR/$1 $DESTDIR$2
+    else
+        cp -RL $SRC_DIR/$1 $DESTDIR$2
+    fi
 }
 
 for i in ${INSTALL_SET[@]}; do
     SRC=$`echo $i | cut -d: -f1`
     DST=`echo $i | cut -d: -f2`
-
     eval SRC_FILES=$SRC
-
     for f in $SRC_FILES; do
         do_file $f $DST
     done
