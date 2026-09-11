bfb81ab7a11c93d622cd5630b22a6f505490ec8a
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-11 23:39:27 +0530

    fix(hyperv): install hot migration hardening

--- install.sh
+++ install.sh
@@ -3,18 +3,8 @@
 # -------------------------------------------------------------------------- #
 # Copyright 2002-2025, OpenNebula Project, OpenNebula Systems                #
 #                                                                            #
-# Licensed under the Apache License, Version 2.0 (the "License"); you may    #
-# not use this file except in compliance with the License. You may obtain    #
-# a copy of the License at                                                   #
-#                                                                            #
-# http://www.apache.org/licenses/LICENSE-2.0                                 #
-#                                                                            #
-# Unless required by applicable law or agreed to in writing, software        #
-# distributed under the License is distributed on an "AS IS" BASIS,          #
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.   #
-# See the License for the specific language governing permissions and        #
-# limitations under the License.                                             #
-#--------------------------------------------------------------------------- #
+# Licensed under the Apache License, Version 2.0.                             #
+# -------------------------------------------------------------------------- #
 
 ARGS=$*
 
@@ -91,6 +81,7 @@ ONE_CLI_LIB_FILES="esxi_client.rb \
                    esxi_vm.rb \
                    hyperv_helper.rb \
                    hyperv_hot_helper.rb \
+                   hyperv_hot_hardening.rb \
                    netapp_shift_helper.rb \
                    oneswap_helper.rb \
                    oneswap_logger.rb \
@@ -133,6 +124,4 @@ for i in ${INSTALL_SET[@]}; do
     done
 done
 
-# Files created through downstream source-delivery APIs may not retain the
-# executable bit. Enforce it on installed command entrypoints.
 chmod +x "$DESTDIR$BIN_LOCATION/oneswap" "$DESTDIR$BIN_LOCATION/oneswap-hyperv" "$DESTDIR$BIN_LOCATION/sesparse"
