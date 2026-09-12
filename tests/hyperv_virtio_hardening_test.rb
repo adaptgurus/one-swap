014e752dfa12b336b21e17376ae304d0e20c66c5
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-12 09:34:53 +0530

    test(hyperv): verify guest-agent MAC binding

--- tests/hyperv_virtio_hardening_test.rb
+++ tests/hyperv_virtio_hardening_test.rb
@@ -24,4 +24,34 @@ class HyperVVirtIOHardeningTest < Minitest::Test
       ENV['VIRTIO_WIN'] = old if old
     end
   end
+
+  def test_expected_guest_macs_are_canonical_and_sorted
+    coordinator = OneSwapHyperV::HotCoordinator.allocate
+    coordinator.instance_variable_set(:@options, {
+      :expected_guest_macs => '00-15-5D-01-02-04,00:15:5d:01:02:03,00:15:5d:01:02:03'
+    })
+    assert_equal ['00:15:5d:01:02:03', '00:15:5d:01:02:04'],
+                 coordinator.send(:normalized_expected_guest_macs)
+  end
+
+  def test_guest_mac_binding_accepts_hyperv_source_macs_with_extra_guest_virtual_mac
+    coordinator = OneSwapHyperV::HotCoordinator.allocate
+    coordinator.instance_variable_set(:@options, {
+      :expected_guest_macs => '00:15:5d:01:02:03,02:42:ac:11:00:02'
+    })
+    metadata = { 'NICs' => [{ 'MacAddress' => '00155D010203' }] }
+    assert coordinator.send(:validate_guest_mac_binding!, metadata)
+  end
+
+  def test_guest_mac_binding_rejects_wrong_agent_or_changed_source_mac
+    coordinator = OneSwapHyperV::HotCoordinator.allocate
+    coordinator.instance_variable_set(:@options, {
+      :expected_guest_macs => '00:15:5d:01:02:04'
+    })
+    metadata = { 'NICs' => [{ 'MacAddress' => '00-15-5D-01-02-03' }] }
+    error = assert_raises(OneSwapHyperV::Error) do
+      coordinator.send(:validate_guest_mac_binding!, metadata)
+    end
+    assert_match(/guest agent MAC inventory does not match/, error.message)
+  end
 end
