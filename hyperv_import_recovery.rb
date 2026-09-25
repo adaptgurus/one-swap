473f18ab4093c221407160da32d243acbded103e
adaptgurus <73125939+adaptgurus@users.noreply.github.com>
2026-09-25 10:54:36 +0000

    fix(hyperv): expose MORPHING prelaunch recovery helper

--- hyperv_import_recovery.rb
+++ hyperv_import_recovery.rb
@@ -492,3 +492,19 @@ module OneSwapHyperV
         end
     end
 end
+
+
+class OneSwapHelper
+    def hyperv_hot_recover_morphing_prelaunch(vm_name, options)
+        options = options.merge(:name => vm_name, :format => 'raw')
+        @options = options
+        @options[:name] = vm_name
+        @options[:context] ||= '/usr/share/one/context'
+        @options[:virt_tools] ||= '/usr/local/share/virt-tools'
+        @options[:img_wait] ||= 120
+        @options[:context_min_free] ||= 1024
+        @options[:context_timeout] ||= 600
+        @hyperv_source_host = OneSwapHyperV::ConnectionProfile.from_options(options).host
+        OneSwapHyperV::HotCoordinator.new(self, options).recover_morphing_prelaunch
+    end
+end
